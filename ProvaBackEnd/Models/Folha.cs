50a8154fd827d32f86415fc361da63a75494c3ac
aluno <Aluno@PR02-302-15.CSED.LAB>
2023-10-06 09:15:37 -0300

    Criaçao Models

---
+++ ProvaBackEnd/Models/Folha.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProvaBackEnd.Models;
+public class Folha
+{
+    public int FolhaId { get; set; }
+    public double ValorHora { get; set; }
+    public int QuantidadeHoras { get; set; }
+    public int Mes { get; set; }
+    public int Ano { get; set; }
+    public double SalarioBruto { get; set; }
+    public double ImpostoRenda { get; set; }
+    public double Inss { get; set; }
+    public double Fgts { get; set; }
+    public double SalarioLiquido { get; set; }
+    public Funcionario? Funcionario { get; set; }
+public Folha()
+{
+    // Construtor Vazio
+}
+
+}
---
+++ ProvaBackEnd/Models/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvaBackEnd.Models;
+public class Funcionario
+{
+    public int FuncionarioId { get; set; }
+    public string? Nome { get; set; }
+    public string? Cpf { get; set; }
+
+public Funcionario()
+{
+    // Construtor Vazio
+}
+}
