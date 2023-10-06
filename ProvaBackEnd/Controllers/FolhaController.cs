b68ccd23a41fde9309a9e31d9ae0cb4ee713ada1
aluno <Aluno@PR02-302-14.CSED.LAB>
2023-10-06 10:11:51 -0300

    Controller Folha

--- ProvaBackEnd/Controllers/FolhaController.cs
+++ ProvaBackEnd/Controllers/FolhaController.cs
@@ -1,5 +1,71 @@
-﻿namespace ProvaBackEnd;
-public class FolhaController
-{
+﻿using ProvaBackEnd.Data;
+using ProvaBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
 
+namespace ProvaBackEnd.Controllers;
+[ApiController]
+[Route("api/folha")]
+public class FolhaController:ControllerBase
+{
+    private readonly AppDataContext _ctx;
+
+    public FolhaController(AppDataContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Folha NovaFolha)
+    {
+        try
+        {
+            _ctx.Folhas.Add(NovaFolha);
+            _ctx.SaveChanges();
+            return Created("", NovaFolha);
+
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    [HttpGet("listar")]
+    public IActionResult Listar()
+    {
+        try
+        {
+
+            var folhas = _ctx.Folhas.ToList();
+
+            return folhas.Count == 0? NotFound("Nenhuma FOLHA encontrado.") : Ok(folhas);
+
+        }
+        catch(Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    [HttpGet("buscar/{cpf}/{mes}/{ano}")]
+    public IActionResult Buscar([FromRoute] string cpf, int mes, int ano)
+    {
+        try
+        {
+            Folha? folhaCadastrada = _ctx.Folhas.FirstOrDefault(x => x.Funcionario.Cpf == cpf && x.Mes == mes && x.Ano == ano);
+            if (folhaCadastrada != null)
+            {
+                return Ok(folhaCadastrada);
+            }
+            else
+            {
+                return NotFound("Folha not found.");
+            }
+
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+    
 }
--- ProvaBackEnd/Controllers/FuncionarioController.cs
+++ ProvaBackEnd/Controllers/FuncionarioController.cs
@@ -1,5 +1,46 @@
-﻿namespace ProvaBackEnd;
-public class FuncionarioController
-{
+﻿using Microsoft.AspNetCore.Mvc;
+using ProvaBackEnd.Data;
+using ProvaBackEnd.Models;
 
+namespace ProvaBackEnd.Controllers;
+[ApiController]
+[Route("api/funcionario")]
+public class FuncionarioController:ControllerBase
+{
+    private readonly AppDataContext _ctx;
+    public FuncionarioController(AppDataContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    [HttpPost]
+    [Route("cadastrar")]
+    public ActionResult Cadastrar([FromBody] Funcionario NovoFunc){
+        try
+        {
+            _ctx.Funcionarios.Add(NovoFunc);
+            _ctx.SaveChanges();
+            return Created("", NovoFunc);
+        }
+        catch (Exception e)
+        {
+            
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpGet]
+    [Route("listar")]
+    public ActionResult Listar(){
+        try
+        {
+            var funcionarios = _ctx.Funcionarios.ToList();
+            if(funcionarios.Count == 0){
+                return NotFound("Nenhum Funcionário Encontrado");
+            }
+            return funcionarios.Count == 0? NotFound():Ok(funcionarios);
+
+        }catch(Exception e){
+            return BadRequest(e);
+        }
+    }
 }
